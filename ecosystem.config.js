00cd2dedd22fcf82c58fe1eee6c3efe8010cf293
v-xiaqingpeng1 <v-xiaqingpeng1@360.cn>
2025-12-13 11:18:43 +0800

    配置pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,12 +3,13 @@ module.exports = {
     {
       name: 'example-picooc',
       script: 'npm',
-      args: 'run start',
+      args: 'run start -- --host=0.0.0.0',
       exec_mode: 'cluster',
       instances: 'max',
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
+      cwd: '/www/wwwroot/egg-example-picooc/',
       env: {
         NODE_ENV: 'production'
       },
